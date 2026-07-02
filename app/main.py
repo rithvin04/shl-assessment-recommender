2bfb3364b55480e76a03c0a731d12b2f50595792
rthvin04 <rithvinrajdyavathi@gmail.com>
2026-07-03 02:56:54 +0530

    Load retriever on startup

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
-
 from app.routes import router
 from app.retriever import Retriever
 
 app = FastAPI()
 
-retriever = Retriever()
-
-app.state.retriever = retriever
-
 app.include_router(router)
+
+
+@app.on_event("startup")
+async def startup():
+    print("Loading retriever...")
+    app.state.retriever = Retriever()
+    print("Retriever loaded.")
